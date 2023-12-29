5da4407555c4e69262d48c369854c5bcbdd6c600
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-12-29 23:57:22 +0530

    lb

--- polymorphism/latebinfding.cpp
+++ polymorphism/latebinfding.cpp
@@ -1,46 +1,56 @@
 #include<iostream>
 using namespace std;
-class person{
-    public:
-        virtual void give(){
-            cout<<"BUN";
-        }
+
+class person {
+public:
+    virtual void give() {
+        cout << "BUN";
+    }
 };
 
-class boy:public person{
-    public :
-        void give(){
-            cout<<" BROWN BUN "<<endl;
-        }
+class boy : public person {
+public:
+    void give() override {
+        cout << " BROWN BUN " << endl;
+    }
 };
 
-class girl : public person{
-    public:
-        void give(){
-            cout<<" PINK BUN "<<endl;
-        }
+class girl : public person {
+public:
+    void give() override {
+        cout << " PINK BUN " << endl;
+    }
 };
 
-int main()
-{
+int main() {
     boy b1;
     girl g1;
-    person *ptr=NULL;
+    person* ptr = NULL;
     int n;
-    cout<<"Enter the choice:\n1 for boy \n2 for girl "<<endl;
-    cin>>n;
-    switch (n)
-    {
-    case 1:
-        ptr=&b1;
-        ptr->give();
-        break;
-    case 2:
-        ptr=&g1;
-        ptr->give();
-        break;
-    default:
-        break;
+
+    while (true) {
+        cout << "Enter the choice:\n1 for boy \n2 for girl \n0 to exit" << endl;
+        cin >> n;
+
+        if (n == 0) {
+            cout << "Exiting program." << endl;
+            break;
+        }
+
+        switch (n) {
+        case 1:
+            ptr = &b1;
+            ptr->give();
+            break;
+        case 2:
+            ptr = &g1;
+            ptr->give();
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
     }
+
     return 0;
 }
