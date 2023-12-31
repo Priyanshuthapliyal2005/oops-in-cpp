489aad9a3a913c5e117a5e39a40bde4cdf9eb310
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-12-31 23:47:22 +0530

    add n numbers in template

--- template/add.cpp
+++ template/add.cpp
@@ -1,59 +1,42 @@
 #include<iostream>
-#include<bits/stdc++.h>
+#include<vector>
 using namespace std;
 
 template<typename T>
-T add(T a, T b)
-{
-    return a + b;
-}
-
-template<typename T>
-T add(T a, T b, T c)
-{
-    return a + b + c;
-}
-
-
-int main()
-{
-    int n;
-    cout <<"ENTER \n1: for addition of two numbers \n2: for addition of three numbers\n0: for exiting the program \n";
-    cin>>n;
-    while(n>=0)
-    {
-        switch(n)
-        {
-            case 1:
-            {
-                int a,b;
-                cout<<"Enter two numbers: ";
-                cin>>a>>b;
-                cout<<"Sum of two numbers is: "<<add(a,b)<<endl;
-                break;
-            }
-            case 2:
-            {
-                int a,b,c;
-                cout<<"Enter three numbers: ";
-                cin>>a>>b>>c;
-                cout<<"Sum of three numbers is: "<<add(a,b,c)<<endl;
-                break;
-            }
-            case 0:
-            {
-                cout<<"Exiting..."<<endl;
-                return 0;
-            }
-
-            default:
-            {
-                cout<<"Invalid input"<<endl;
-                break;
-            }
-        }
-        cout <<"ENTER \n1: for addition of two numbers \n2: for addition of three numbers \n";
-        cin>>n;
+T add(const vector<T>& numbers) {
+    T sum = 0;
+    for (const T& num : numbers) {
+        sum += num;
     }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "ENTER \n'input how many numbers you want to add': for addition of 'n' numbers \n0: for exiting the program \n";
+    
+    while (true) {
+        cout << "Enter your choice: ";
+        cin >> n;
+
+        if (n == 0) {
+            cout << "Exiting..." << endl;
+            break;
+        }
+
+        if (n < 1) {
+            cout << "Invalid input" << endl;
+            continue;
+        }
+
+        vector<int> numbers(n);
+        cout << "Enter " << n << " numbers: ";
+        for (int i = 0; i < n; ++i) {
+            cin >> numbers[i];
+        }
+
+        cout << "Sum of " << n << " numbers is: " << add(numbers) << endl;
+    }
+
     return 0;
 }
